871ddfb3931eb7abd6efba9c4e6f8bebfcdcb7cf
Imad Mohamad <yehya.mh@live.com>
2024-05-20 18:00:12 +0200

    Creat Comments

--- Part1.py
+++ Part1.py
@@ -3,12 +3,18 @@
 x = 25
 y = 3.13
 z = "Advanced Python"
-is_active = 0
+is_active = False
 
         # 2
+# I added formatting to the numbers to make the output clearer.
 print("{:.2f}".format(x / y))
 print(str(x) + z)
-print(bool(is_active))
+ # Or
+ # print(str(x), z, sep="")
+is_active = not is_active
+print(is_active)
+
+print("------------")
 
         # Task 2
         # 1
@@ -22,7 +28,14 @@ print("-".join(split_text))
         # 3
 numbers = list(range(10, 101, 10))
 squares = [i ** 2 for i in numbers]
+""" OR
+squares = []
+for number in numbers:
+    squares.append(number ** 2)
+"""
 print(squares)
+
+print("------------")
 
         # Task 3
         # 1
@@ -42,12 +55,14 @@ unique_numbers.add(6)
 unique_numbers.remove(2)
 print(3 in unique_numbers)
 
+print("------------")
+
         # Task 4
         # 1
 def is_prime(n):
     lst = [2, 3, 5, 7]
     for item in lst:
-        if n % item == 0:
+        if (n == 1) or (n not in lst and n % item == 0):
             return False
     return True
  # I created a list to make the numbers on the screen appear clearer.
@@ -59,6 +74,8 @@ print(lst_prime_numbers)
 
         # 2
 list_numbers = list(range(1, 16))
+ # I created a list to make the numbers on the screen appear clearer.
+list_results = []
 for number in list_numbers:
     print_string = ""
     if number % 3 == 0:
@@ -66,6 +83,7 @@ for number in list_numbers:
     if number % 5 == 0:
         print_string += "Buzz"
     if not print_string:
-        print(number)
+        list_results.append(number)
     else:
-        print(print_string)
+        list_results.append(print_string)
+print(list_results)
